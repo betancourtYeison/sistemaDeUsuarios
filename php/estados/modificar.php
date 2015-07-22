4eda83f2d4e709aaa7caff2c4d4ebf2f244e8b79
jorgekast18 <jacr011893@gmail.com>
2015-07-22 14:30:13 -0500

    modificar

---
+++ php/estados/modificar.php
@@ -0,0 +1,46 @@
+<?php
+	include("../lib/session.php");	
+	
+	$miEstado = new Estado( $_POST['codigo'], $_POST['descripcion'], $_POST['tipo'] );							
+	$miEstado->setConexion( $conexion );				
+?>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="description" content="Proyecto de CreArteWeb para Sistemas de usuarios" />
+		<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1"/>
+		<title>Módulo Modificar</title>	
+		<META HTTP-EQUIV="REFRESH" CONTENT="2;URL=http://localhost/sistemaDeUsuarios/php/estados/estados.php"> 
+		<link rel = "shortcut icon" type="image/x-icon" href = "../../img/favicon.ico" />	
+		<link href = "../../css/estilo.css" rel="stylesheet" type="text/css" />		
+		<link href="../../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	</head>	
+	<body>	
+		<div class="container">	
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id='cabecera'>
+				<?php
+					include('../lib/barraUsuario.php');
+				?>
+			</div>		
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id='menu'>
+				<?php
+					include('../lib/menu.php');
+				?>	
+			</div>	
+			<div class="page-header" align = "center">
+				<image src='../../img/modulo.png' class='img-responsive'>
+				<h2>MODIFICAR MÓDULO</h2>
+				<?php 
+					$miEstado->modificarEstado( $_POST['codigo'] ); 
+				?>
+
+			</div>
+			<div id="footer" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<?php
+					include('../lib/footer.php');
+				?>	
+			</div>	
+		</div>	
+	</body>
+</html>
